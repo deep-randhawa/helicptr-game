d0012db1ce178a0c720f016f0a332062801c76c0
deepsaran <deepsaranr@gmail.com>
2014-02-24 15:30:43 -0500

    made the obstacles appear at a random position

--- helicptr_game/ViewController.h
+++ helicptr_game/ViewController.h
@@ -10,6 +10,7 @@
 
 int Y;
 BOOL Start;
+int RandomPosition;
 
 @interface ViewController : UIViewController
 
@@ -19,6 +20,26 @@ BOOL Start;
     IBOutlet UILabel *intro3;
     
     IBOutlet UIImageView *Heli;
+    
+    IBOutlet UIImageView *Obsctacle1;
+    IBOutlet UIImageView *Obsctacle2;
+    
+    IBOutlet UIImageView *Bottom1;
+    IBOutlet UIImageView *Bottom2;
+    IBOutlet UIImageView *Bottom3;
+    IBOutlet UIImageView *Bottom4;
+    IBOutlet UIImageView *Bottom5;
+    IBOutlet UIImageView *Bottom6;
+    IBOutlet UIImageView *Bottom7;
+    
+    IBOutlet UIImageView *Top1;
+    IBOutlet UIImageView *Top2;
+    IBOutlet UIImageView *Top3;
+    IBOutlet UIImageView *Top4;
+    IBOutlet UIImageView *Top5;
+    IBOutlet UIImageView *Top6;
+    IBOutlet UIImageView *Top7;
+    
     
     NSTimer *timer;
     
