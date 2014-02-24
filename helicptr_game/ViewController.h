00dcb30ccda1103c3881104662a0a310ab8a6958
deepsaran <deepsaranr@gmail.com>
2014-02-24 13:37:25 -0500

    Initial Commit

---
+++ helicptr_game/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  helicptr_game
+//
+//  Created by Epic.deep on 2/24/14.
+//  Copyright (c) 2014 deepsaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ helicptr_game/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  helicptr_game
+//
+//  Created by Epic.deep on 2/24/14.
+//  Copyright (c) 2014 deepsaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
